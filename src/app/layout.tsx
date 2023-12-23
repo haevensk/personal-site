b6fcce940f3ddaf5bace2ba6145d7aeeccce19b7
Haeven Kelley <haevenshane@gmail.com>
2023-12-22 19:08:54 -0600

    Revert "Rebase"
    This reverts commit 0c44115071aa0cf739a06aea604b092a2736ee9d.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'SubHire',
-  description: 'Hire verified subcontractors',
+  title: 'Haeven Devs',
+  description: 'Get to know me',
 }
 
 export default function RootLayout({
