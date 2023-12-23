47d03492e424d59999b1b5965e41dc3cc2d68d7d
Haeven Kelley <haevenshane@gmail.com>
2023-12-23 16:48:18 -0600

    Revises spacing further

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,17 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [initialLoad, setInitialLoad] = useState(false);
+
   useEffect(() => {
     if (!initialLoad)
       var typed = new Typed('#typed', {
-        strings: ["Engineer. Designer. Leader.", 'Student. Dreamer. Mentor.', 'Brother. Husband. Friend.'],
+        strings: ['Engineer. Designer. Leader.', 'Student. Dreamer. Mentor.', 'Brother. Husband. Friend.'],
         typeSpeed: 90
       });
 
       setInitialLoad(true);
   }, [setInitialLoad, initialLoad]);
+
   return (
     <div className={styles.wrapper}>
         <span id="typed" className={styles.typed}>
