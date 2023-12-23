24c32da425cdd22ab2c9de9afb289e4ca4050321
Haeven Kelley <haevenshane@gmail.com>
2023-12-22 18:50:45 -0600

    Attempts to fix broken deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 'use client'
-import Image from 'next/image'
+// import Image from 'next/image'
 import styles from './page.module.css'
-import Typed from 'typed.js';
+// import Typed from 'typed.js';
 import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [initialLoad, setInitialLoad] = useState(false);
-  useEffect(() => {
-    if (!initialLoad)
-      var typed = new Typed('#typed', {
-        strings: ["Engineer. Designer. Leader.", 'Student. Dreamer. Mentor.', 'Brother. Husband. Friend.'],
-        typeSpeed: 90
-      });
-      setInitialLoad(true);
-  }, [setInitialLoad, initialLoad]);
+  // const [initialLoad, setInitialLoad] = useState(false);
+  // useEffect(() => {
+  //   if (!initialLoad)
+  //     var typed = new Typed('#typed', {
+  //       strings: ["Engineer. Designer. Leader.", 'Student. Dreamer. Mentor.', 'Brother. Husband. Friend.'],
+  //       typeSpeed: 90
+  //     });
+  //     setInitialLoad(true);
+  // }, [setInitialLoad, initialLoad]);
   return (
     <div className={styles.wrapper}>
         <span id="typed" className={styles.typed}>
