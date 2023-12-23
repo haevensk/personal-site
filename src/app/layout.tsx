0c44115071aa0cf739a06aea604b092a2736ee9d
Haeven Kelley <haevenshane@gmail.com>
2023-12-22 19:07:02 -0600

    Rebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Haeven Devs',
-  description: 'Get to know me',
+  title: 'SubHire',
+  description: 'Hire verified subcontractors',
 }
 
 export default function RootLayout({
