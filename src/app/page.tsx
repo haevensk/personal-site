e7f8fb375a08d53e280a181ca33eda75f0b58ae5
Haeven Kelley <haevenshane@gmail.com>
2023-12-27 15:09:54 -0600

    Adds github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
   return (
     <div className={styles.wrapper}>
         <span id="typed" className={styles.typed} />
+        <a>github.com/Haeven</a>
     </div>
   )
 }
