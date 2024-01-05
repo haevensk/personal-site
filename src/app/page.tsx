9e0135b9caedf5956a2dadd414ed34f718ada352
Haeven Kelley <haevenshane@gmail.com>
2024-01-04 18:12:07 -0600

    Adds email and SMS links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,9 @@ export default function Home() {
         <div style={{margin: 'auto', display: 'flex', flexDirection: 'column', gap: '2rem', marginTop: '7rem'}}>
           <a target='0' href="/me.cv.pdf" style={{textAlign: 'center', color: '#9E9E9E', cursor: 'pointer'}}>View my CV</a>
           <a target='0' href="https://github.com/Haeven" style={{textAlign: 'center', color: '#9E9E9E', cursor: 'pointer'}}>View my GitHub</a>
-          <a target='0' href="https://x.com/hvndevs" style={{textAlign: 'center', color: '#9E9E9E', cursor: 'pointer'}}>Follow me on 𝕏</a>
+          <a target='0' href="https://x.com/hvndevs" style={{textAlign: 'center', color: '#9E9E9E', cursor: 'pointer'}}>Follow my 𝕏</a>
+          <a target='0' href="haevendevs@gmail.com" style={{textAlign: 'center', color: '#9E9E9E', cursor: 'pointer'}}>Contact Email</a>
+          <a target='0' href="sms:+16082082300" style={{textAlign: 'center', color: '#9E9E9E', cursor: 'pointer'}}>Contact SMS</a>
           {/* <Image className={styles.githubStat} sizes={'(max-width: 768px) 100vw, auto (max-width: 1200px) 467px, 195px'} src="/stats1.svg" alt="me" width="467" height="195"/> */}
           {/* <Image className={styles.githubStat} sizes={'(max-width: 768px) 100vw, auto (max-width: 1200px) 467px, 195px'} src="/stats2.svg" alt="me" width="467" height="195"/> */}
           {/* <Image className={styles.githubStat} sizes={'(max-width: 768px) 100vw, auto (max-width: 1200px) 467px, 195px'} src="/stats3.svg" alt="me" width="467" height="195"/> */}
